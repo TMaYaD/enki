5dfc9720ba12314cd4d3352ff6ea8986b896a9b9
Subhash Chandra <TMaYaD@loonyb.in>
2010-04-06 09:28:06 +0530

    fixed up home page for hemingway theme

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def index
     @tag = params[:tag]
-    @posts = Post.find_recent(:tag => @tag, :include => :tags)
+    @all_posts = Post.find_recent(:tag => @tag, :include => :tags, :limit => 12)
+    @posts = @all_posts[0..1]
+    @more_posts = @all_posts[2..12]
 
     raise(ActiveRecord::RecordNotFound) if @tag && @posts.empty?
 
@@ -14,5 +16,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {:include => [:approved_comments, :tags]}))
     @comment = Comment.new
+
+    @more_posts = Post.find_recent(:tag => @tag, :include => :tags, :limit => 10)
   end
 end
